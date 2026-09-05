b54c3289942114908b80ded6d81ce6ca8adf0b9a
Andy Hart <wahart27613@gmail.com>
2026-09-05 18:51:26 -0500

    fixed the file to remove hard paths

--- src/main.cpp
+++ src/main.cpp
@@ -33,76 +33,53 @@
 
 #include "Arduino.h"
 
-#define CUSTOM_CHAR_HEADER  // this must be done prior to #include of HomeSpan call anywhere. 
-#include "elapsedMillis.h"
-// #include "HomeSpan.h" 
-// #include "ESP32CAN.h"
+#define CUSTOM_CHAR_HEADER  // this must be done prior to #include of HomeSpan call anywhere.
 #include "CAN_config.h"
-
-// #include "config.h"
 
 #include <CoachWifi.h>
 #include <CoachESP32.h>
 #include <DeviceFactory.h>
 #include <Packet.h>
 #include <PacketQueue.h>
+
 CAN_device_t CAN_cfg;
 
 void setup() {
 	printf("setup start\n");
-	// put your setup code here, to run once:
-  	pinMode(LED_BUILTIN, OUTPUT);
-  	printf("Setup complete, LED pin set to OUTPUT.\n");
-	
-	constexpr uint8_t ERROR = 1;
-	bool completed = false;
-	CoachESP32* coachESP32 = CoachESP32::getInstance(&CAN_cfg);
-	if (coachESP32 != nullptr) {
-		printf("CoachESP32 instance created/obtained \n");
-	
-		// Initialize the CoachESP32 instance
-		coachESP32->initialize();
-		CoachWifi* coachWifi = CoachWifi::getInstance();
-		if (coachWifi != nullptr) { 
-			printf("CoachWifi instance created/obtained \n");
-			
-			coachWifi->initialize(); 
-			printf("setup: DeviceFactory getting instance \n"); 
-			DeviceFactory* factory = DeviceFactory::getInstance();
-			if (factory != nullptr) {
-				// printf("ERRROR: factory failed to create devices \n");
-				completed = true;
-				printf("%u: Init complete.\n", (uint32_t)millis());
-				// setting it to print mode for now while I gather raw data so I can determine instance data
-				Packet::setPacketPrintMode(packetPrintYes);
-				Packet::initialize();
+	pinMode(LED_BUILTIN, OUTPUT);
 
-			} 
-		}
-	} 
-	/*
-	if (!completed) {
-		printf("ERROR: *********initialization failure!!!************ \n");
-		exit(ERROR);
-	} */
+	CoachESP32* coachESP32 = CoachESP32::getInstance(&CAN_cfg);
+	if (coachESP32 == nullptr) {
+		printf("ERROR: CoachESP32 init failed\n");
+		return;
+	}
+	printf("CoachESP32 instance created/obtained\n");
+	coachESP32->initialize();
+
+	CoachWifi* coachWifi = CoachWifi::getInstance();
+	if (coachWifi == nullptr) {
+		printf("ERROR: CoachWifi init failed\n");
+		return;
+	}
+	printf("CoachWifi instance created/obtained\n");
+	coachWifi->initialize();
+
+	printf("setup: DeviceFactory getting instance\n");
+	DeviceFactory* factory = DeviceFactory::getInstance();
+	if (factory == nullptr) {
+		printf("ERROR: DeviceFactory init failed\n");
+		return;
+	}
+
+	printf("%u: Init complete.\n", (uint32_t)millis());
+	// Packet dump on while gathering raw RV-C instance data
+	Packet::setPacketPrintMode(packetPrintYes);
+	Packet::initialize();
 	printf("setup complete\n");
 }
 
-int i = 0;
 void loop() {
 	PacketQueue::adjustTimingOfPacketRecieve();
-	
 	CoachWifi::pollSpan();
-	// printf("looping %u\n", i);
-	i++;
-	// CAN_frame_t packet;
 	CoachESP32::pollESP32();
-	// put your main code here, to run repeatedly:
-	//digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level
-  	// printf("LED is ON.\n");
-  	//delay(100);                       // wait for a second
-  	//digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  	// printf("LED is OFF.\n");
-  	//delay(100);                       // wait for a second
-
 }
